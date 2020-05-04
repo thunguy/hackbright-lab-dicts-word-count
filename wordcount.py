d0f451d52f1e9ee00d00266c0dbec64a6f211b4f
Thu Nguyen <thunguy@me.com>
2020-05-04 14:07:43 -0700

    generated word_count dict | print 'word: count' | sys.argv

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,26 @@
-# put your code here.
+import sys
+
+def word_count(filename):
+
+    file = open(sys.argv[1])
+
+    counter = {}
+
+    for line in file:
+        line = line.rstrip()
+        words = line.split(" ")
+
+        for word in words:
+            counter[word] = counter.get(word, 0) + 1
+
+    return counter
+
+def display_word_count(counter):
+
+    for word, count in counter.items():
+        # print (word, count)
+        print (f"{word}: {count}")
+
+# print(word_count(sys.argv))
+print(display_word_count(word_count(sys.argv)))
+
